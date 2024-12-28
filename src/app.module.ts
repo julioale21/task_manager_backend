95988710b8d2db4a9f88759dc199331dcf45a545
julioale21 <30907777+julioale21@users.noreply.github.com>
2024-12-28 10:20:49 -0300

    extracted .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TasksModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://julioale04031981:IllRhEXGE0QkImIL@cluster0.jmuny.mongodb.net/coally-db?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
   controllers: [AppController],
   providers: [AppService],
