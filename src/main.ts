cda03883023a4085cc2c28f3650f0c6cceebe74f
julioale21 <30907777+julioale21@users.noreply.github.com>
2024-12-28 23:19:44 -0300

    updated main

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,3 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ValidationPipe } from '@nestjs/common';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   // Enabled all origins only for development purposes
-//   app.enableCors({
-//     origin: '*',
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//     allowedHeaders: '*',
-//   });
-
-//   const config = new DocumentBuilder()
-//     .setTitle('Coally Task Manager API')
-//     .setDescription(
-//       'The Coally Task Manager API has the purpose of serve as a simple task manager for the Coally technical test.',
-//     )
-//     .setVersion('1.0')
-//     .addTag('coally-task-manager')
-//     .build();
-
-//   const document = SwaggerModule.createDocument(app, config);
-//   SwaggerModule.setup('api', app, document);
-
-//   app.useGlobalPipes(
-//     new ValidationPipe({
-//       whitelist: true,
-//       forbidNonWhitelisted: true,
-//       transform: true,
-//       transformOptions: { enableImplicitConversion: true },
-//     }),
-//   );
-
-//   await app.listen(process.env.PORT || 3001);
-// }
-// bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -44,52 +5,35 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
 
+  // Enabled all origins only for development purposes
   app.enableCors({
     origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Coally Task Manager API')
+    .setDescription(
+      'The Coally Task Manager API has the purpose of serve as a simple task manager for the Coally technical test.',
+    )
+    .setVersion('1.0')
+    .addTag('coally-task-manager')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
+      transformOptions: { enableImplicitConversion: true },
     }),
   );
 
-  const config = new DocumentBuilder()
-    .setTitle('Fiction Exress RESTFul API')
-    .setDescription('Fiction Express API endpoints description')
-    .setVersion('1.0')
-    .build();
-
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/v1', app, documentFactory);
-
-  await app.init();
-
-  const server = app.getHttpServer();
-  return server;
+  await app.listen(process.env.PORT || 3001);
 }
-
-// Manejo Serverless
-let cachedServer;
-
-export default async function handler(req, res) {
-  if (!cachedServer) {
-    cachedServer = await bootstrap();
-  }
-  return cachedServer(req, res);
-}
-
-// Solo para desarrollo local
-if (require.main === module) {
-  bootstrap().then((server) => {
-    server.listen(3002, () => {
-      console.log('Server running on port 3002');
-    });
-  });
-}
+bootstrap();
